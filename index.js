1b83335df81e3a61a05ab08ed9a8ef71fdf69dc5
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-03 17:22:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    input.addEventListener('click', function() {
+      // Handle the event here
+    });
+  }
+  
+
+  function addingEventListener() {
+    const input = document.getElementById('input');
+  
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+
+
+  function addingEventListener() {
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+
+
+
+  function addingEventListener() {
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();
+  
+  
+  
